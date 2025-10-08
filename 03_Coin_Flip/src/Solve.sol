5b610bf38c45fa2a9334d39ccf5c4fb90d72b462
Kiracoon <kiramyby@gmail.com>
2025-10-08 19:37:01 +0800

    style: forge fmt

--- 03_Coin_Flip/src/Solve.sol
+++ 03_Coin_Flip/src/Solve.sol
@@ -5,8 +5,7 @@ import "./CoinFlip.sol";
 
 contract Solve {
     CoinFlip public coinFlip;
-    uint256 FACTOR =
-        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _coinFlip) {
         coinFlip = CoinFlip(_coinFlip);
